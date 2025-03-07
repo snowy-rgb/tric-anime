f8e60e694abb8d11ffb3203d71fa88d2f788f4a2
snowy <trigger.snowii@gmail.com>
2025-03-08 01:54:28 +0900

    Update setTheme.js

--- system/js/commands/setTheme.js
+++ system/js/commands/setTheme.js
@@ -3,12 +3,12 @@ function setTheme(themeName) {
 
     // 테마별 이미지 매핑
     let themes = {
-        "기본": "../../../assets/inGameImages/storyBoxes/w-n.svg",
-        "생각": "../../../assets/inGameImages/storyBoxes/w-th.svg",
-        "놀람": "../../../assets/inGameImages/storyBoxes/w-sh.svg",
-        "t:기본": "../../../assets/inGameImages/storyBoxes/tBox-n.svg",
-        "t:생각": "../../../assets/inGameImages/storyBoxes/tBox-th.svg",
-        "t:놀람": "../../../assets/inGameImages/storyBoxes/tBox-sh.svg",
+        "기본": "./assets/inGameImages/storyBoxes/w-n.svg",
+        "생각": "./assets/inGameImages/storyBoxes/w-th.svg",
+        "놀람": "./assets/inGameImages/storyBoxes/w-sh.svg",
+        "t:기본": "./assets/inGameImages/storyBoxes/tBox-n.svg",
+        "t:생각": "./assets/inGameImages/storyBoxes/tBox-th.svg",
+        "t:놀람": "./assets/inGameImages/storyBoxes/tBox-sh.svg",
     };
 
     // 기존 테마 제거 후 새로운 테마 적용
